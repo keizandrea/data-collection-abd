651914300f595f59791950aba0b5c43960726b42
Keiza <keizaandrea@gmail.com>
2026-06-13 23:36:19 +0700

    delete unused file

--- web_scrapping.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-page_to_scrape = requests.get("https://karir.com/search-lowongan?keyword=Business%20Development")
-soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-job_type = soup.findAll()
